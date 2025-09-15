ffc80cd33d0824ed4c478255deeaf01cbd43ab71
Chris Chin cachin <cachin@uwe.nsa.gov>
2025-09-15 18:29:29 +0000

    get docker compose working

--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,7 +1,11 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
+import path from "path";
 
-export const config: NextConfig = {};
+export const config: NextConfig = {
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+};
 
 export const withAnalyzer = (sourceConfig: NextConfig): NextConfig =>
   withBundleAnalyzer()(sourceConfig);
